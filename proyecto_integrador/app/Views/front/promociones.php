0b693255e129dd3692c9a1a30f8c36baa483143a
Natalia Micaela Romero <nataliamicaela1616@gmail.com>
2023-11-12 09:10:45 -0300

    Actualizacion

---
+++ proyecto_integrador/app/Views/front/promociones.php
@@ -0,0 +1,22 @@
+<?php 
+for($1=0;$1<=7;$1++){
+	echo"<div class='card' style='width: 18rem;'>
+  <img src='...' class='card-img-top' alt='...'>
+  <div class='card-body'>
+    <h5 class='card-title'>Promocion $1</h5>
+    <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href='#' class='btn btn-primary'>Go somewhere</a>
+  </div>
+</div>";
+
+
+
+
+
+
+}
+
+
+
+
+?>
